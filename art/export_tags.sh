8a331c14fe6c4f88a3a92e2911c6798c51dd76d2
LeHaine <colt.daily@gmail.com>
2020-11-04 08:21:18 -0500

    add export_tags shell script

---
+++ art/export_tags.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+aseprite="D:/Program Files/Aseprite/Aseprite.exe"
+fileName=""
+excludeFiles=("gun" "cursor")
+for file in ./ase/*.aseprite ; do
+  fileName="${file##*/}"
+  fileName="${fileName%.*}"
+  if [[ ! " ${excludeFiles[@]} " =~ " ${fileName} " ]]; then
+  "$aseprite" -b $file --save-as ./export_tiles/${fileName}{tag}0.png
+  fi
+done
